d113c00567908a9fcaa341e9971b5ff3400810f7
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-13 12:06:52 -0300

    feat: new scafollded

---
+++ BlazorApp-Treinamento/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp_Treinamento.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Tittle { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
