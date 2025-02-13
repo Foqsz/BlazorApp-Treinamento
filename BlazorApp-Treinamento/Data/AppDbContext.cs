ad2899bc8c84ab0b056786117981a3d5e2310517
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-13 12:06:19 -0300

    feat: banco de dados em memoria

---
+++ BlazorApp-Treinamento/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BlazorApp_Treinamento.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp_Treinamento.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; } = null!;
+}
+
--- BlazorApp-Treinamento/Program.cs
+++ BlazorApp-Treinamento/Program.cs
@@ -1,10 +1,20 @@
 using BlazorApp_Treinamento.Components;
+using BlazorApp_Treinamento.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+    options.UseInMemoryDatabase("App"));
+
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -14,6 +24,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
